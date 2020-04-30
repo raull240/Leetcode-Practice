5712386b16c06f043fb0698f30b2d6177f9d8877
raull240 <rahulrao@juniper.net>
2020-04-29 23:01:52 -0700

    Bitwise AND of Numbers Range

---
+++ Practice-Problems-Leetcode/src/thirtyDaysofCode/BitwiseANDofNumbersRange.java
@@ -0,0 +1,30 @@
+package thirtyDaysofCode;
+
+// Bitwise AND of Numbers Range 
+
+/*
+  Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+Example 1:
+
+Input: [5,7]
+Output: 4
+Example 2:
+
+Input: [0,1]
+Output: 0
+  */
+public class BitwiseANDofNumbersRange {
+
+	public int rangeBitwiseAnd(int m, int n) {
+		int shift = 0;
+		// find the common 1-bits
+		while (m < n) {
+			m >>= 1;
+			n >>= 1;
+			++shift;
+		}
+		return m << shift;
+	}
+
+}
